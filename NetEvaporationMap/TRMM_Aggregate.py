1be8eda638a104f9ef783a2e18663203d05092e8
isamarcortes <40585590+isamarcortes@users.noreply.github.com>
2022-02-08 17:20:37 -0500

    Update TRMM_Aggregate.py

--- NetEvaporationMap/TRMM_Aggregate.py
+++ NetEvaporationMap/TRMM_Aggregate.py
@@ -1,19 +1,25 @@
 import rasterio as rio
 from rasterio.enums import Resampling
-import os
 import glob
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-TRMM_Files = sorted(glob.glob('/Users/isamarcortes/Desktop/TRMM_GeoTIFF/*.tif'))
-downscale_factor = 1/16
-files = []
+'''
+Code Documentation:
+    This code takes any number of 0.25 by 0.25 degree TRMM data and 
+    aggregates to a 1 by 1 degree using the resampling average function.
+    It then takes the average of all of the files and creates one average
+    precipitation map.
+'''
+
+
+TRMM_Files = sorted(glob.glob('/Users/isamarcortes/Desktop/TRMM_GeoTIFF/*.tif')) #change this to working directory
+downscale_factor = 1/16 #downscale to 1 degree resolution
+files = [] 
 
 for j in TRMM_Files:
     files.append(j)
 
-test = []
+NPArrays = []
 
 for i in files: 
         with rio.open(i) as dataset:
@@ -25,9 +31,21 @@ for i in files:
             ),
             resampling=Resampling.average
         )    
-        test.append(data)
+        NPArrays.append(data)
 
-test1 = np.mean(test,axis = 0)
+AverageMap = np.mean(NPArrays,axis = 0)
 
 
-#test1 = np.add(test)/len(test)
+with rio.open(j) as src:
+    raster_data = src.read()
+    raster_meta = src.profile
+
+# make any necessary changes to raster properties, e.g.:
+raster_meta['dtype'] = "float32"
+raster_meta['nodata'] = -99
+
+with rio.open('/Users/isamarcortes/Desktop/TRMM_GeoTIFF/CopyOfDataInCaseIMessUp copy/AverageTRMM.tif', 'w', **raster_meta) as dst:
+    dst.write(AverageMap)
+
+     
+        
