eefc43b8b4fcab66e324dafc867b18de3392af0d
isamarcortes <40585590+isamarcortes@users.noreply.github.com>
2022-02-08 15:44:33 -0500

    Update TRMM_Aggregate.py

--- NetEvaporationMap/TRMM_Aggregate.py
+++ NetEvaporationMap/TRMM_Aggregate.py
@@ -8,17 +8,26 @@ import matplotlib.pyplot as plt
 
 TRMM_Files = sorted(glob.glob('/Users/isamarcortes/Desktop/TRMM_GeoTIFF/*.tif'))
 downscale_factor = 1/16
+files = []
+
+for j in TRMM_Files:
+    files.append(j)
+
+test = []
+
+for i in files: 
+        with rio.open(i) as dataset:
+            data = dataset.read(
+            out_shape=(
+                dataset.count,
+                int(dataset.height * downscale_factor),
+                int(dataset.width * downscale_factor)
+            ),
+            resampling=Resampling.average
+        )    
+        test.append(data)
+
+test1 = np.mean(test,axis = 0)
 
 
-for i in TRMM_Files:
-    with rio.open(i) as dataset:
-        data = dataset.read(
-        out_shape=(
-            dataset.count,
-            int(dataset.height * downscale_factor),
-            int(dataset.width * downscale_factor)
-        ),
-        resampling=Resampling.bilinear
-    )
-
-plt.imshow(data[0,:,:])
+#test1 = np.add(test)/len(test)
